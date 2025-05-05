72020b507662845c019ad6eed55bc670fe13fc35
IT24101174 <it24101174@my.sliit.lk>
2025-05-05 21:44:53 +0530

    Edit file path

--- src/main/java/com/janani/prettytouch/constVar/FIleConst.java
+++ src/main/java/com/janani/prettytouch/constVar/FIleConst.java
@@ -1,7 +1,7 @@
 package com.janani.prettytouch.constVar;
 
 public class FIleConst {
-    public static final String FILE_PATH = "C:\\Users\\jakli\\Desktop\\PrettyTouch\\dataFile\\";
+    public static final String FILE_PATH = "C:\\Users\\Asus\\Documents\\GitHub\\PrettyTouch\\dataFile\\";
     public static final String USER_FILE = "users.csv";
     public static final String[] USER_HEADERS = {"id","createdBy","createdAt","status","username","password","firstName","lastName","role","phoneNumber","dob"};
     public static final String SERVICE_FILE = "services.csv";
