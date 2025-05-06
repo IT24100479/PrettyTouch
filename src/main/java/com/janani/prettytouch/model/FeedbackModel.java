c12b2ad0ed9273cad4641a2d69dcd13fa0b04c79
It24101344 <it24101344@my.sliit.lk>
2025-05-06 20:28:37 +0530

    Add Appointement Id variable create get appoinment ID and Set appoinment ID

--- src/main/java/com/janani/prettytouch/model/FeedbackModel.java
+++ src/main/java/com/janani/prettytouch/model/FeedbackModel.java
@@ -1,19 +1,29 @@
 package com.janani.prettytouch.model;
 
+import com.janani.prettytouch.services.AppointmentService;
 import com.janani.prettytouch.util.TypeConverter;
 
 public class FeedbackModel extends Model {
     private int rating;
     private String comment;
+    private int AppointmentId;
 
-    public FeedbackModel(String id, String createdBy, String createdAt, String status, String rating, String comment) {
+    public FeedbackModel(String id, String createdBy, String createdAt, String status, String rating, String comment,String appointmentId) {
         super(id, createdBy, createdAt, status);
         setRating(rating);
+        setAppointmentId(appointmentId);
         this.comment = comment;
     }
 
-    public FeedbackModel() {
-        super();
+    public FeedbackModel() {super();}
+
+    public int getAppointmentId(){return AppointmentId;}
+
+    public AppointmentModel getAppointment(){
+        return (AppointmentModel) AppointmentService.getInstance().getById(AppointmentId);
+    }
+    public void setAppointmentId(String appointmentId){
+        AppointmentId = TypeConverter.stringToInt(appointmentId);
     }
 
     @Override
