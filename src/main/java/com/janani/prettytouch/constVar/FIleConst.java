713366b19631f4c2ae977aec62288ab18b50cd29
JananiAmaya <jakliyanage@gmail.com>
2025-05-09 14:00:44 +0530

    data file fix

--- src/main/java/com/janani/prettytouch/constVar/FIleConst.java
+++ src/main/java/com/janani/prettytouch/constVar/FIleConst.java
@@ -3,7 +3,7 @@ package com.janani.prettytouch.constVar;
 public class FIleConst {
     public static final String FILE_PATH = "C:\\Users\\Garawa\\IdeaProjects\\PrettyTouch\\dataFile\\";
     public static final String USER_FILE = "users.csv";
-    public static final String[] USER_HEADERS = {"id","createdBy","createdAt","status","username","password","firstName","lastName","role","phoneNumber","dob"};
+    public static final String[] USER_HEADERS = {"id","createdBy","createdAt","status","username","password","firstName","lastName","role","phoneNumber","dob","email"};
     public static final String SERVICE_FILE = "services.csv";
     public static final String[] SERVICE_HEADERS = {"id","createdBy","createdAt","status","type","serviceName","description","printPrice","realPrice","imageUrl","isOffer"};
     public static final String APPOINTMENT_FILE = "appointments.csv";
