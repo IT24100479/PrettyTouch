31ec205080fa029983345ce2bd646dfda48278e9
JananiAmaya <jakliyanage@gmail.com>
2025-05-16 00:40:17 +0530

    error fix

--- src/main/java/com/janani/prettytouch/servlet/feedback/create.java
+++ src/main/java/com/janani/prettytouch/servlet/feedback/create.java
@@ -6,6 +6,7 @@ import com.janani.prettytouch.model.UserModel;
 import com.janani.prettytouch.services.FeedbackService;
 import com.janani.prettytouch.util.TypeConverter;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,23 +14,23 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-public class create {
-    public class Create extends HttpServlet {
+@WebServlet("/feedback/create")
+public class Create extends HttpServlet {
 
-        @Override
-        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            HttpSession session = req.getSession(true);
-            UserModel user = (UserModel) session.getAttribute("user");
-            if (user != null && GlobalConst.USER_TYPE_USER.equalsIgnoreCase(user.getRole())) {
-                FeedbackService feedbackService = FeedbackService.getInstance();
-                FeedbackModel feedback = new FeedbackModel();
-                feedback.setAppointmentId(req.getParameter("aid"));
-                feedback.setRating(req.getParameter("rating"));
-                feedback.setComment(req.getParameter("comment"));
-                feedback.setCreatedBy(TypeConverter.intToString(user.getId()));
-                feedback.setStatus("1");
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(true);
+        UserModel user = (UserModel) session.getAttribute("user");
+        if (user != null && GlobalConst.USER_TYPE_USER.equalsIgnoreCase(user.getRole())) {
+            FeedbackService feedbackService = FeedbackService.getInstance();
+            FeedbackModel feedback = new FeedbackModel();
+            feedback.setAppointmentId(req.getParameter("aid"));
+            feedback.setRating(req.getParameter("rating"));
+            feedback.setComment(req.getParameter("comment"));
+            feedback.setCreatedBy(TypeConverter.intToString(user.getId()));
+            feedback.setStatus("1");
 
-                if(feedback.validate()){
+            if(feedback.validate()){
 
                     if(feedbackService.add(feedback)){
                         String msg = "Rating is Successfully Added";
@@ -40,14 +41,12 @@ public class create {
                     }
 
 
-                }else{
-                    String error = "Required Fields Are Empty";
-                    resp.sendRedirect(feedbackService.createReturnUrl(req.getContextPath(),"/feedback/createFeedback.jsp",feedback,error));
-                }
             }else{
-                resp.sendRedirect(req.getContextPath()+"/user/logout");
+                String error = "Required Fields Are Empty";
+                resp.sendRedirect(feedbackService.createReturnUrl(req.getContextPath(),"/feedback/createFeedback.jsp",feedback,error));
             }
+        }else{
+            resp.sendRedirect(req.getContextPath()+"/user/logout");
         }
     }
-
 }
